717604061cecbed6999bbd7a28f58d4d671ef6f1
jsichon <Sich@iMac.local>
2016-02-14 23:58:25 -0500

    Hw1 - Part 2

---
+++ HW1-Part2/HW1-Part2/HW1-Part2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW1-Part2
+//
+//  Created by Juan Sichon on 2/14/16.
+//  Copyright © 2016 Juan Sichon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
